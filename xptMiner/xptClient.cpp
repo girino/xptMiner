580d4ab1b9381cb80385fa5f80302d387083011c
girino <girinovey@girino.org>
2014-03-01 22:57:07 -0300

    cygwin also does not give error on disconnect. r == 0 check needs to be present on any OS

--- xptMiner/xptClient.cpp
+++ xptMiner/xptClient.cpp
@@ -509,11 +509,8 @@ bool xptClient_process(xptClient_t* xptClient)
 	if( r <= 0 )
 	{
 		// receive error, is it a real error or just because of non blocking sockets?
-		if( WSAGetLastError() != WSAEWOULDBLOCK
-#ifdef __linux__
-		|| r == 0
-#endif
-		  ) // closes the "if". Linux does not give an error on disconnect
+		// Linux does not send error on disconnect, so if r == 0, disconect
+		if( WSAGetLastError() != WSAEWOULDBLOCK || r == 0 )
 		{
 			xptClient->disconnected = true;
 			return false;
